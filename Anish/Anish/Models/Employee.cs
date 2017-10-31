23de0a72dec0f2de8077df0b3c117485e8ce1da2
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-10-31 09:17:22 -0600

    Part 3

---
+++ Anish/Anish/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Anish.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public int Name { get; set; }
+        public int Department { get; set; }
+    }
+}
