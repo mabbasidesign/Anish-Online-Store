1c9461656215cea682d2bdec4986c2b45b71695e
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-11-22 14:39:57 -0700

    Optimizing refrence

--- Anish/Anish/Global.asax.cs
+++ Anish/Anish/Global.asax.cs
@@ -16,6 +16,8 @@ namespace Anish
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
