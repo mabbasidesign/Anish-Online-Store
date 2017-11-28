16f7441f837683bcd322e122ea53def7c62da031
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-11-28 13:55:30 -0700

    Save image to database

--- Anish/Anish/Controllers/ProductController.cs
+++ Anish/Anish/Controllers/ProductController.cs
@@ -16,6 +16,12 @@ namespace Anish.Controllers
             return View();
         }
 
+        public ActionResult IndexSQL()
+        {
+
+            return View();
+        }
+
 
         public JsonResult ImageUpload(ProductViewModel model)
         {
@@ -32,15 +38,37 @@ namespace Anish.Controllers
 
         public JsonResult ImageSQL(ProductViewModel model)
         {
+            var db = new MVCTutorialEntities();
             var file = model.ImageFile;
+            int imageId = 0;
+            byte[] imageByte = null;
+
             if (file != null)
             {
-                var fileName = Path.GetFileName(file.FileName);
-                var extention = Path.GetExtension(file.FileName);
-                var filenamewithoutextension = Path.GetFileNameWithoutExtension(file.FileName);
                 file.SaveAs(Server.MapPath("/UploadedImage/" + file.FileName));
+
+                BinaryReader reader = new BinaryReader(file.InputStream);
+                imageByte = reader.ReadBytes(file.ContentLength);
+                ImageStore img = new ImageStore();
+                img.ImageName = file.FileName;
+                img.ImageByte = imageByte;
+                img.ImagePath = "/UploadedImage/" + file.FileName;
+                img.IsDeleted = false;
+                db.ImageStores.Add(img);
+                db.SaveChanges();
+
+                imageId = img.ImageId;
             }
-            return Json(file.FileName, JsonRequestBehavior.AllowGet);
+
+            return Json(imageId, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ImageRetrieve(int imgID)
+        {
+            var db = new MVCTutorialEntities();
+            var img = db.ImageStores.SingleOrDefault(x => x.ImageId == imgID);
+
+            return File(img.ImageByte, "image/JPG");
         }
 
 
