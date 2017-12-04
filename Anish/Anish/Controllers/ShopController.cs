dae5eb9523673804b7cf8a5f574381c4fc869c2f
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-12-04 14:21:21 -0700

    display shopping checbox

---
+++ Anish/Anish/Controllers/ShopController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Anish.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Anish.Controllers
+{
+    public class ShopController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new MVCTutorialEntities();
+            var itemList = new List<MyShop>();
+            itemList.Add(new MyShop { ItemId = 1, ItemName = "Rice", IsAvailable = true, });
+            itemList.Add(new MyShop { ItemId = 2, ItemName = "Pulse", IsAvailable = false, });
+            itemList.Add(new MyShop { ItemId = 3, ItemName = "Salt", IsAvailable = false, });
+            itemList.Add(new MyShop { ItemId = 4, ItemName = "Sugar", IsAvailable = true, });
+            itemList.Add(new MyShop { ItemId = 5, ItemName = "Soap", IsAvailable = false, });
+            itemList.Add(new MyShop { ItemId = 6, ItemName = "Book", IsAvailable = true, });
+
+            ViewBag.ItemList = itemList;
+
+            return View();
+        }
+    }
+}
---
+++ Anish/Anish/Models/MyShop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Anish.Models
+{
+    public class MyShop
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
